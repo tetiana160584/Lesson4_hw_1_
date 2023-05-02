8c7d72619656531fc0837e0eb5a3207bef4f71b0
tetiana160584 <tetiana.kostyhina@gmail.com>
2023-05-02 22:55:27 +0300

    Lesson_11_HW_4

--- src/main/java/org/example/Lesson11/Lesson_11.java
+++ src/main/java/org/example/Lesson11/Lesson_11.java
@@ -1,8 +1,10 @@
 package org.example.Lesson11;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 
 public class Lesson_11 {
     public static void main(String[] args) {
@@ -10,14 +12,21 @@ public class Lesson_11 {
         info.put(20, "value_2");
         info.put(10, "value_1");
         info.put(30, "value_3");
-        printMapValues(info);
-    }
-    public static void printMapValues (Map<Integer, String>map){
-    TreeMap<Integer,String>sortedMap=new TreeMap<>(map);
-        for (Map.Entry<Integer, String> entry:sortedMap.entrySet()){
-           System.out.println("key"+entry.getKey()+"="+entry.getValue());
 
+
+        List<Map.Entry<Integer, String>> sortedEntries = info.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toList());
+
+        for (Map.Entry<Integer, String> entry : sortedEntries) {
+            System.out.println("key" + entry.getKey() + "="+ entry.getValue());
         }
 
+        }
     }
-}
+
+
+
+
+
