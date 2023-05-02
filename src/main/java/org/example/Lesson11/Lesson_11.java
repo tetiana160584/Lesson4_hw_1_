008fd18f0da1815d0d920d5b51e3ed144522f86a
tetiana160584 <tetiana.kostyhina@gmail.com>
2023-05-02 22:29:46 +0300

    Lesson_11_HW_3

--- src/main/java/org/example/Lesson11/Lesson_11.java
+++ src/main/java/org/example/Lesson11/Lesson_11.java
@@ -1,13 +1,11 @@
 package org.example.Lesson11;
 
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Lesson_11 {
     public static void main(String[] args) {
-
         Map<Integer, String> info = new HashMap<>();
         info.put(20, "value_2");
         info.put(10, "value_1");
