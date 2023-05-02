de949a6d8fc61fc01c03fd9705ffd912bacc0a66
tetiana160584 <tetiana.kostyhina@gmail.com>
2023-05-02 22:25:01 +0300

    Lesson_11_HW_2

--- src/main/java/org/example/Lesson11/Lesson_11.java
+++ src/main/java/org/example/Lesson11/Lesson_11.java
@@ -9,12 +9,15 @@ public class Lesson_11 {
     public static void main(String[] args) {
 
         Map<Integer, String> info = new HashMap<>();
-        info.put(20, "value_2"  );
+        info.put(20, "value_2");
         info.put(10, "value_1");
-        info.put(30,"value_3");
-        Map<Integer,String>sortedInfo=new TreeMap<>(info);
-       for (Map.Entry<Integer, String> entry:sortedInfo.entrySet()){
-           System.out.println(entry.getKey()+"="+entry.getValue());
+        info.put(30, "value_3");
+        printMapValues(info);
+    }
+    public static void printMapValues (Map<Integer, String>map){
+    TreeMap<Integer,String>sortedMap=new TreeMap<>(map);
+        for (Map.Entry<Integer, String> entry:sortedMap.entrySet()){
+           System.out.println("key"+entry.getKey()+"="+entry.getValue());
 
         }
 
